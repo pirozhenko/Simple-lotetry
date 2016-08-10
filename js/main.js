4bfb870451c692709d8178d1fea3fdeb14a6f901
pirozhenko Alexandr <pirog_13@ukr.net>
2016-08-10 16:32:53 +0300

    change and send

--- js/main.js
+++ js/main.js
@@ -4,10 +4,7 @@ var app = {
             regExForMail: /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, // определяет регулярное выражение для валидации имени
             regExForPhone: /\([0-9]{3}\)\s[0-9]{3}-[0-9]{4}/, // определяет регулярное выражение для валидации телефона
             invalidClass: 'invalid',
-            invalidClassMsg: 'invalid-msg',
             validClass: 'valid',
-            delClass: 'delete',
-            textForNullInput: 'This value is requaried!',
             textForEmail: 'Enter valid mail',
             textForPhone: 'Enter valid phone: (063) 555-5555',
             countStrTable: 1,
@@ -15,21 +12,21 @@ var app = {
 
         },
 
-        validationEmail: function( elem ) {
+        validationEmail: function( elem ) {//Валидация поля для почты
            value = elem.value;
            regexp = this.settings.regExForMail;
 
            return regexp.test( value );
         },
 
-        validationPhone: function( elem ) {
+        validationPhone: function( elem ) {//Валидация для телефона
             value = elem.value;
             regexp = this.settings.regExForPhone;
 
            return regexp.test( value );
         },
 
-        success: function(parent, input, check) {
+        success: function(parent, input, check) {//Добавление зелёной рамки и галочки
             var invalidClass    = this.settings.invalidClass,
                 validClass      = this.settings.validClass;
 
@@ -39,7 +36,7 @@ var app = {
             check.addClass('fa fa-check');
         },
 
-        fail: function( parent ) {
+        fail: function( parent ) {//Добавление текста ошибки
             var invalidClass    = this.settings.invalidClass,
                 validClass      = this.settings.validClass;
 
@@ -109,6 +106,8 @@ var app = {
         addEl: function ( e ) {//нажатие на кнопку сохранить
 
             var countStrTable = this.settings.countStrTable,
+                email  = document.getElementById('email'),
+                phone  = document.getElementById('phone'),
                 arrName       = this.settings.arrName,
                 tr            = document.createElement( 'tr' ),
                 th            = document.createElement( 'th' ),
@@ -118,7 +117,7 @@ var app = {
 
 
             for ( var i = 0; i < form.length-1; i++ ) {//создание стольбцов и вставка их в таблицу с вместимостью инпутов
-                if ( form[i].value == ''  ) {
+                if ( form[i].value == '' ) {
 
                     form[i].parentNode.addClass(this.settings.invalidClass);
                     invalidFields += 1;
@@ -126,7 +125,7 @@ var app = {
                 }
             }
 
-            if ( invalidFields == 0 ) {
+            if ( (invalidFields == 0) && ( this.validationEmail(email) ) && ( this.validationPhone(phone) ) )  {
                 tbody.appendChild( tr );
                 tr.appendChild( th );
                 th.innerHTML = this.settings.countStrTable;
@@ -150,17 +149,23 @@ var app = {
                 spanRem  = document.createElement( 'span' ),
                 span     = document.createElement( 'span' );
 
-            input.setAttribute('size', '85%');
+            input.setAttribute('size', '50%');
             boot.insertBefore( span, input );
             span.className = 'tag label label-info';
             span.innerHTML = arrName[rand];//запись случайного имени в инпут
 
             span.appendChild( spanRem );
             spanRem.dataset.role = 'remove';
+
+            spanRem.addEventListener("click", function(e) {
+               e.preventDefault();
+               app.removeSpan(e);
+           });
         },
 
-        removeSpan: function ( span ) {//удаление случайного елемента
-            parentNode.remove( spa );
+        removeSpan: function ( e ) {//удаление случайного елемента
+           var elem = e.target.parentNode;
+           elem.remove();
         },
 
         init: function() {
@@ -185,11 +190,6 @@ var app = {
                 document.getElementById('new').addEventListener("click", function(e) {
                     e.preventDefault();
                     app.winner(e);
-                });
-
-                spanRem.addEventListener("click", function(e) {
-                    e.preventDefault();
-                    app.removeSpan(span);
                 });
 
             Element.prototype.hasClass = function (className) {
